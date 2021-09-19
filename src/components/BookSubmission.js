424cb74ee217dd5de6b3f7bbcd54ee7d07b790fb
Tom Keech <t0m.k33ch@gmail.com>
2021-09-18 21:21:01 -0400

    User can upload books
    User can now upload books to the DB

--- src/components/BookSubmission.js
+++ src/components/BookSubmission.js
@@ -9,21 +9,34 @@ import "react-awesome-button/dist/styles.css";
 
 export default function BookSubmission() {
     const [yourChips, setYourChips] = useState([])
+    const [disableButton, setDisableButton] = useState(true)
     function handleAddChip(chip) {
         setYourChips(oldArray => [...oldArray, chip]);
+        if (disableButton == true) {
+            setDisableButton(false)
+        }
     }
 
     function handleDeleteChip(chip, index) {
         setYourChips(yourChips.filter(item => item !== chip));
+            if (yourChips.length == 1) {
+                setDisableButton(true)
+            }
     }
 
-    function doSomethingThenCall(next) {
-        fetch('http://localhost:8000/customer/')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            next();
-        });
+    function doSomethingThenCall(next) { // Post the books in the DB
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                book_name: yourChips,
+                id: '',
+                user_id: '4175c756-1fcc-4b13-8755-6fd7e48d323c'
+            })
+        };
+        fetch('http://localhost:8000/books/', requestOptions)
+            .then(response => response)
+            .then(data => next());
     }
     return (
         <div style={{ display: 'flex', width: '50%', flexDirection: 'column' }}>
@@ -34,6 +47,7 @@ export default function BookSubmission() {
             />
             <div style = {{ marginTop: '10%', display: 'flex',  justifyContent:'center', alignItems:'center' }}>
             <AwesomeButtonProgress
+                disabled={disableButton}
                 type="secondary"
                 size="medium"
                 action={(element, next) => doSomethingThenCall(next)}
